833cfb8a7d0b44bf9f9a8a50de375e9c6444bc6e
Daniel_Sisovic <siskoftn@gmail.com>
2020-11-21 21:30:40 +0100

    Updated base href

--- js/modules/http.js
+++ js/modules/http.js
@@ -7,7 +7,7 @@ class Http {
     githubJobs = [];
     currentFetchPage = 1;
     githubJobsUrl = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
-    githubJobDetailUrl = '';
+    githubJobDetailUrl = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions/';
 
     constructor() { }
 
